9d5bae940f7d6db6aa7f1bca077db51f80eebf95
claudiaceradavis <claudiaceradavis@outlook.com>
2022-10-01 15:49:02 -0400

    First Commit 10/1 - FTC Example Code and Drive Code in TestOPMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOPModule.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+
+public abstract class TestOPModule extends LinearOpMode {
+//       private Gyroscope imu;
+//       private DcMotor motorTest ;
+//       private DigitalChannel digitalTouch;
+//       private DistanceSensor sensorColorRange;
+//       private Servo servoTest;
+
+    private DcMotor leftMotor = null ;
+    private DcMotor rightMotor = null;
+
+       public void runOpMode() {
+//           imu = hardwareMap.get(Gyroscope.class, "imu");
+//           motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+//           digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+//           sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+//           servoTest = hardwareMap.get(Servo.class, "servoTest");
+//TODO: Add initialization code as necessary
+           DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+           DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+           DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+           DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+           // Reverse the right side motors
+           // Reverse left motors if you are using NeveRests
+           motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+           motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+           telemetry.addData("Status", "Initialized");
+           telemetry.update();
+           // Wait for the game to start (driver presses PLAY)
+           waitForStart();
+
+           // run until the end of the match (driver presses STOP)
+           if (isStopRequested()) return;
+
+           while (opModeIsActive()) {
+               double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+               double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+               double rx = gamepad1.right_stick_x;
+
+               // Denominator is the largest motor power (absolute value) or 1
+               // This ensures all the powers maintain the same ratio, but only when
+               // at least one is out of the range [-1, 1]
+               double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+               double frontLeftPower = (y + x + rx) / denominator;
+               double backLeftPower = (y - x + rx) / denominator;
+               double frontRightPower = (y - x - rx) / denominator;
+               double backRightPower = (y + x - rx) / denominator;
+
+               motorFrontLeft.setPower(frontLeftPower);
+               motorBackLeft.setPower(backLeftPower);
+               motorFrontRight.setPower(frontRightPower);
+               motorBackRight.setPower(backRightPower);
+
+
+
+
+
+
+
+           }
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+
