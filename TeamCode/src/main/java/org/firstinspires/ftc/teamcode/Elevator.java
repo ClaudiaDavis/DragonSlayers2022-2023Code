2af85e157afc2b0be908b551817f63db489b2c89
claudiaceradavis <claudiaceradavis@outlook.com>
2022-12-17 14:06:28 -0500

    12/17 -- Elevator Class Updated (ADAM)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator.java
@@ -1,17 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class Elevator {
 
         // Motor objects for the two motors that drive the elevator
         private DcMotor motor1;
         private DcMotor motor2;
+        Gamepad gamepad2 = new Gamepad();
 
-        public Elevator(DcMotor m1, DcMotor m2) {
+
+        public Elevator(Gamepad g, DcMotor m1, DcMotor m2) {
             // Initialize the two motor objects
             motor1 = m1;
             motor2 = m2;
+            gamepad2 = g;
         }
 
         public void moveUp() {
@@ -22,13 +26,30 @@ public class Elevator {
             // Start both motors
             //motor1.start();
             //motor2.start();
-        }
+            }
 
-        private int joystickThreshold = 5;
+    public void moveDown() {
+        // Set the direction of both motors to "down"
+        motor1.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Start both motors
+        //motor1.start();
+        //motor2.start();
+    }
+
+
+        int joystickThreshold = 5;
+        boolean running;
+        //double y = gamepad2.left_stick_y; // Remember, this is reversed!
+        double currentPosition = motor1.getCurrentPosition();
+        double MAXROTATION = 100.0; //change when know actual number
+
 
         public void moveWithJoystick(){
             while(running) {
-                joystickValue = gamepad1.;
+                double joystickValue = gamepad2.left_stick_y;
+                        //get joy;
                 if (joystickValue < joystickThreshold*(-1)) {
                     moveDown();
                 }
@@ -43,6 +64,12 @@ public class Elevator {
                 }
             }
         }
+
+
+        public boolean comeBackHome() {
+        boolean doNotMove = false;
+        sleep(3000);
+         }
 
         /*
 
@@ -72,20 +99,9 @@ public class Elevator {
         }
         */
 
-        public boolean comeBackHome() {
-            boolean doNotMove = false;
-            sleep(3000);
-        }
 
-        public void moveDown() {
-            // Set the direction of both motors to "down"
-            motor1.setDirection(DcMotorSimple.Direction.REVERSE);
-            motor2.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            // Start both motors
-            //motor1.start();
-            //motor2.start();
-        }
+
 
        // public void stop() {
             // Stop both motors
